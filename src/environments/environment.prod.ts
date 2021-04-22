4165adbeb4cf5f8a7f3673eb788dd3748bd8f8da
Duy Phuong <phuong.vd@consultindochina.com>
2021-04-22 15:02:01 +0700

    update all

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 export const environment = {
-    production: true,
-    API_URL_EVALUETE: 'https://li1jm77bc8.execute-api.ap-southeast-1.amazonaws.com/prod/leave'
+  production: true,
+  API_URL_EVALUETE:
+    'https://li1jm77bc8.execute-api.ap-southeast-1.amazonaws.com/prod/leave',
+  CHAT_SOCKET_ENPOINT:
+    'wss://7o5p7mfv40.execute-api.ap-southeast-1.amazonaws.com/production',
+  reconnectInterval: 5000,
 };
