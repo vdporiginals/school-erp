37cc78a5c7b4e254786762c2f2b9b62d62856d44
Duy Phuong <phuong.vd@consultindochina.com>
2021-04-22 13:32:38 +0700

    update full

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { filter, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  currentUrl = new BehaviorSubject<string>(null);
 
-  constructor() {}
+  constructor(private router: Router, public route: ActivatedRoute) {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        distinctUntilChanged()
+      )
+      .subscribe(() => {
+        this.checkIsHiddenRoute();
+      });
+  }
 
+  checkIsHiddenRoute() {
+    console.log(this.router.url);
+
+    if (this.router.url.includes('detail-message')) {
+      this.currentUrl.next('detail-message');
+    } else {
+      this.currentUrl.next(null);
+    }
+  }
 }
