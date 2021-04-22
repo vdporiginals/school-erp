5c4669a6f86ef73073dafa9c0e34cd5151fa65a7
Duy Phuong <phuong.vd@consultindochina.com>
2021-04-22 11:24:18 +0700

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 export const environment = {
     production: true,
     API_URL_EVALUETE: 'https://li1jm77bc8.execute-api.ap-southeast-1.amazonaws.com/prod/leave'
