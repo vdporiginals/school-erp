872f66a0ad50d2884d45f3232e761993f10bfbb0
Duy Phuong <phuong.vd@consultindochina.com>
2021-04-22 14:30:22 +0700

    fix route

--- src/app/message/message.page.ts
+++ src/app/message/message.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../storage/localstorage.service';
 
 @Component({
   selector: 'app-message',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.page.scss'],
 })
 export class MessagePage implements OnInit {
+  curUser: any = this.storage.getToken();
 
-  constructor() { }
+  constructor(private storage: LocalStorageService) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
