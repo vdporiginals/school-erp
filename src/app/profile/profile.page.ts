b9d41c3bb61a819bb261c511b1d1fe9d13f6564a
Duy Phuong <phuong.vd@consultindochina.com>
2021-04-22 13:41:51 +0700

    fix all

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalStorageService } from '../storage/localstorage.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  constructor(private storage: LocalStorageService, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  logout() {
+    this.storage.clear();
+    this.router.navigateByUrl('/login');
   }
-
 }
